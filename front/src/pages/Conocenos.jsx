4b63e58ca5cbef254d344c4ea69a687dd3e22f8f
EL_Padrino <65505303+aberrospic@users.noreply.github.com>
2023-06-14 02:02:51 -0500

    conocenos(falta redes)

--- front/src/pages/Conocenos.jsx
+++ front/src/pages/Conocenos.jsx
@@ -14,20 +14,43 @@ export default function Conocenos() {
             <div className="conocenos-container">
                 <div className="conocenos-descripcion">
                     {/* <p></p> */}
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, a consequatur quo dolorem labore expedita distinctio perspiciatis doloribus modi necessitatibus eligendi odit voluptates atque totam inventore ipsum magni dolores nihil! Dolores, fugiat. Voluptas id ullam suscipit quibusdam perspiciatis quam dicta, iusto vel delectus, atque tempore iste, quos aspernatur harum?</p>
+                    <p>Somos un equipo de profesionales apasionados por la tecnología y comprometidos con la excelencia en el desarrollo de software. Nos especializamos en diferentes áreas para ofrecer soluciones innovadoras y de calidad a nuestros clientes.<br></br>
+                    Nuestro equipo se caracteriza por su colaboración estrecha, su creatividad y su enfoque en la excelencia técnica. Trabajamos de la mano con nuestros clientes para comprender sus necesidades y ofrecer soluciones personalizadas que impulsen su éxito. 
+                    Estamos comprometidos con la calidad, la innovación y el cumplimiento de los plazos de entrega.
+                    </p>
                 </div>
                 <div className="conocenos-miembros">
                     <div className="miembro">
                         <img src={foto1} alt="foto1" className="foto-img"/>
-                        <p className="nombre">Sebastian Ferreyra</p>
+                        <div className="datos">
+                            <h4 className="datos-nombre">Sebastian Ferreyra</h4>
+                            <p className="datos-descripcion">Bachiller en Ciencia de la Computación (UNI), con experiencia en desarrollo web y un interés particular en el área de DevOps, 
+                            busca combinar su conocimiento técnico con las mejores prácticas de implementación y gestión de proyectos. </p>
+                            <div class="datos-redes">
+                                <div class="redsocial">a</div>
+                                <div class="redsocial">b</div>
+                                <div class="redsocial">c</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="miembro">
                         <img src={foto2} alt="foto1" className="foto-img" />
-                        <p className="nombre">Abraham Berrospi</p>
+                        <div className="datos">
+                            <h4 className="datos-nombre">Abraham Berrospi</h4>
+                            <p className="datos-descripcion">Bachiller en Ciencia de la Computacion(UNI), entusiasta de la tecnología con una pasión por el mundo de Linux, infraestructura y desarrollo full stack. 
+                            Posee experiencia en la administración de sistemas, virtualización y despliegue de aplicaciones. 
+                            Su enfoque versátil le permite trabajar en diferentes capas de una aplicación, desde el back-end hasta el front-end</p>
+                            <p></p>
+                        </div>
                     </div>
                     <div className="miembro">
                         <img src={foto3} alt="foto1" className="foto-img"/>
-                        <p className="nombre">Gabriela Colque</p>
+                        <div className="datos">
+                            <h4 className="datos-nombre">Gabriela Colque</h4>
+                            <p className="datos-descripcion">Bachiller en Ciencia de la Computacion(UNI), se especializa en el campo de la Inteligencia Artificial. 
+                            Con un profundo interés en el aprendizaje automático y el procesamiento del lenguaje natural, busca aplicar sus conocimientos en proyectos innovadores. </p>
+                            <p></p>
+                        </div>
                     </div>
                 </div>
             </div>
