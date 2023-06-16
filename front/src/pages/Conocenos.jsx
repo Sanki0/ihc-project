91c2e8ed8b8bd39131ff7176c8274d55c4d3ca31
EL_Padrino <65505303+aberrospic@users.noreply.github.com>
2023-06-15 20:29:21 -0500

    .

--- front/src/pages/Conocenos.jsx
+++ front/src/pages/Conocenos.jsx
@@ -1,10 +1,8 @@
 import { Link, Outlet } from "react-router-dom"
 import Menu2 from "../components/Menu2";
 import '../styles/Conocenos.css'
-import foto1 from '../assets/foto1.jpg'
-import foto2 from '../assets/foto2.jpg'
-import foto3 from '../assets/foto3.jpg'
 import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
+/* import { FaEquals, FaPlus} from 'react-icons/fa'; */
 
 export default function Conocenos() {
     return (
