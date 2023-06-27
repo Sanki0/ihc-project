27c670e54b5479ae7fb54dd6ce8b3b9eccb4ec9b
EL_Padrino <65505303+aberrospic@users.noreply.github.com>
2023-06-26 19:02:07 -0500

    css acerca

--- front/src/pages/Acerca.jsx
+++ front/src/pages/Acerca.jsx
@@ -11,9 +11,9 @@ export default function Home() {
       <div className="Acerca">
         <h1 className="Acerca-h1 Acerca-h1-1">Acerca de</h1>
         <p className="Acerca-paragraph Acerca-paragraph-1">Hecho con amor por alumnos de la Universidad Nacional de Ingeniería</p>
-        <p className="Acerca-paragraph Acerca-paragraph-2">Creamos esta herramienta para facilitar que los niños sordos y sus padres aprendan el ABC en el lenguaje de señas estadounidense. Deletreo manual.</p>
-        <p className="Acerca-paragraph Acerca-paragraph-3">Aprenda más sobre el <a href="https://www.w3schools.com">lenguaje de señas</a> o has una <a href="https://www.w3schools.com">donación</a></p>
-        <p className="Acerca-paragraph Acerca-paragraph-4">Seguimiento de manos por <a href="https://www.w3schools.com"> </a></p>
+        <p className="Acerca-paragraph Acerca-paragraph-2">Creamos esta herramienta para facilitar que los niños sordos y sus padres aprendan el ABC de la lengua de señas peruana. Deletreo manual.</p>
+        <p className="Acerca-paragraph Acerca-paragraph-3">Aprenda más sobre la <a href="https://repositorio.minedu.gob.pe/handle/20.500.12799/5545">lengua de señas</a> o has una <a href="https://www.ensenasperu.org/involucrate.php">donación</a></p>
+        <p className="Acerca-paragraph Acerca-paragraph-4">Seguimiento de manos por <a href="https://www.w3schools.com">TensorFlow.js</a></p>
       </div>
     </div>
   );
