0b7ace5dcd8ddfc7f7f7a0efd2cd45317658671e
EL_Padrino <65505303+aberrospic@users.noreply.github.com>
2023-06-26 19:33:42 -0500

    menu2-conocenos-reparado

--- front/src/components/Menu2.jsx
+++ front/src/components/Menu2.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom"
-import './styles/Menu2.css'
+import './styles/Menu.css'
 
 export default function Menu(){
     return (
         <div className="Menu2-navbar">
             <div className="Menu2-navbar-left" >
-                <Link to="/">
+                <Link to="/"> 
                     <img src={"src/assets/logo.jpg"} alt="Logo" className="logo-img" />
                 </Link>
             </div>
             <div className="Menu2-navbar-right">
                 <div className="nav2-link-wrapper">
-                    <Link className="nav2-link" to={"/formulario"}>Sugerencias</Link>
+                    <Link className="nav-link" to={"/conocenos"}>Conócenos</Link>
+                </div>
+                <div className="nav2-link-wrapper">
+                    <Link className="nav-link" to={"/acerca"}>Acerca</Link>
                 </div>
             </div>
         </div>
